48675602821dac40a2ae2a1ce831bfbcad4c1b56
Charlyzzz <erwincdl@gmail.com>
2017-01-11 02:45:28 -0300

    User model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule Faster.UserTest do
+  use Faster.ModelCase
+
+  alias Faster.User
+
+  @valid_attrs %{password: "some content", username: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
