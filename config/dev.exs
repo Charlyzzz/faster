67a9bfb63a7a93826731de6e86041c553ca1dccd
Charlyzzz <erwincdl@gmail.com>
2017-01-11 02:43:08 -0300

    Postgres login

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :faster, Faster.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "faster",
+  password: "fasterz",
   database: "faster_dev",
   hostname: "localhost",
   pool_size: 10
