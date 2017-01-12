bbc28342aef99bb13bee80fa04cad262cfc3b761
Charlyzzz <erwincdl@gmail.com>
2017-01-12 02:33:42 -0300

    removed jquery (by now)

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,7 +12,6 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import $ from "jquery"
 
 // Import local files
 //
@@ -20,17 +19,3 @@ import $ from "jquery"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-
-var login_button
-var jumbotron
-
-$(document).ready(function(){
-     login_button = $("#login")
-     jumbotron = $("#jumbotron")
-     login_button.click(login_fadeout)
-})
-
-var login_fadeout = function(event){
-    login_button.fadeOut(400,login_button.remove)
-    jumbotron.animate({height: jumbotron.outerHeight() - login_button.outerHeight() + "px"}, 800)    
-}
