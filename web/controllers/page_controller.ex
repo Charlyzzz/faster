c480001130094b2e7bb6876040f569364dabc634
Charlyzzz <erwincdl@gmail.com>
2017-01-14 05:39:31 -0300

    Logged user greeting

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule Faster.PageController do
   use Faster.Web, :controller
+  alias Faster.Session
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", user: Session.current_user(conn)
   end
 end
